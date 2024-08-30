0415e12899b1099850567f974c2e84892dc3c5bf
Mark Jordan Ugtong <159249841+markjordanugtongspc@users.noreply.github.com>
2024-08-30 12:09:36 +0800

    Add files via upload

---
+++ pokedex.js
@@ -0,0 +1,104 @@
+const pokemonCount = 50; 
+var pokedex = {};
+
+window.onload = async function() {
+
+    createLoader();
+
+    for (let i = 1; i <= pokemonCount; i++) {
+        await getPokemon(i);
+    }
+    updateTable();
+
+    setTimeout(function() {
+        document.getElementById('pokeball-loader').remove();
+    }, 3000);
+
+    document.querySelector('.search-bar').addEventListener('input', function() {
+
+        createLoader();
+
+        let searchTerm = this.value.toLowerCase();
+
+        setTimeout(function() {
+            updateTable(searchTerm);
+            document.getElementById('pokeball-loader').remove();
+        }, 3000);
+    });
+}
+
+function createLoader() {
+
+    let existingLoader = document.getElementById('pokeball-loader');
+    if (!existingLoader) {
+        const loader = document.createElement('div');
+        loader.id = 'pokeball-loader';
+        loader.innerHTML = `
+            <div class="pokeball-loader">
+                <div class="pokeball"></div>
+            </div>
+        `;
+        document.body.appendChild(loader);
+    }
+}
+
+async function getPokemon(num) {
+    let url = "https://pokeapi.co/api/v2/pokemon/" + num.toString();
+    let res = await fetch(url);
+    let pokemon = await res.json();
+
+    let pokemonName = pokemon["name"];
+    let pokemonType = pokemon["types"].map(typeInfo => typeInfo.type.name);
+    let pokemonImg = pokemon["sprites"]["front_default"];
+    let pokemonAbilities = pokemon["abilities"].map(abilityInfo => abilityInfo.ability.name);
+
+    res = await fetch(pokemon["species"]["url"]);
+    let pokemonDesc = await res.json();
+    pokemonDesc = pokemonDesc["flavor_text_entries"].find(entry => entry.language.name === "en").flavor_text;
+
+    pokedex[num] = {
+        "name": pokemonName,
+        "img": pokemonImg,
+        "types": pokemonType,
+        "abilities": pokemonAbilities,
+        "desc": pokemonDesc
+    };
+}
+
+function addPokemonToTable(pokemon) {
+    let tableBody = document.querySelector('.tablee tbody');
+    let row = tableBody.insertRow();
+
+    let cellImg = row.insertCell(0);
+    let imgElement = document.createElement('img');
+    imgElement.src = pokemon["img"];
+    imgElement.alt = pokemon["name"];
+    cellImg.appendChild(imgElement);
+
+    let cellName = row.insertCell(1);
+    cellName.textContent = capitalizeFirstLetter(pokemon["name"]);
+
+    let cellTypes = row.insertCell(2);
+    cellTypes.textContent = pokemon["types"].map(capitalizeFirstLetter).join(', ');
+
+    let cellAbilities = row.insertCell(3);
+    cellAbilities.textContent = pokemon["abilities"].map(capitalizeFirstLetter).join(', ');
+
+    let cellDesc = row.insertCell(4);
+    cellDesc.textContent = pokemon["desc"];
+}
+
+function capitalizeFirstLetter(string) {
+    return string.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
+
+function updateTable(searchTerm = '') {
+    let tableBody = document.querySelector('.tablee tbody');
+    tableBody.innerHTML = ''; 
+
+    let filteredPokedex = Object.values(pokedex).filter(pokemon => 
+        pokemon.name.toLowerCase().includes(searchTerm)
+    );
+
+    filteredPokedex.forEach(pokemon => addPokemonToTable(pokemon));
+}
